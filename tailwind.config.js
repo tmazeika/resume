d084526100f65d7441046ea2f5d822d6a1baaca0
TJ Mazeika <mazeika.tj@gmail.com>
2022-09-08 21:41:54 -0500

    stash

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      serif: ['Lora', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
