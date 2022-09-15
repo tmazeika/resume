0f15566e2b7a8357d77f08cb22d4d2bf7b9cdcee
TJ Mazeika <mazeika.tj@gmail.com>
2022-09-15 09:45:27 -0500

    Simplify styles and spruce up language (#1)
    * stash
    
    * Update style and list item language
    
    * Formatting
    
    * Update .NET and React perf boost language
    
    * Update language
    
    * Rewriting language
    
    * Language updates

---
+++ src/Page.tsx
@@ -0,0 +1,100 @@
+import { ReactNode } from 'react';
+import data from './data';
+
+function Page() {
+  return (
+    <>
+      <div className="space-y-3 font-serif text-sm">
+        <div className="flex justify-between">
+          <div>
+            <h1 className="font-bold text-3xl">{data.name}</h1>
+            <p>{data.position}</p>
+          </div>
+          <div className="flex flex-col items-end">
+            <p>{data.location}</p>
+            <p>{data.email}</p>
+            <div className="flex items-center">
+              <img className="w-4 h-4" src="github.svg" alt="GitHub logo" />
+              <p>/{data.githubUsername}</p>
+            </div>
+          </div>
+        </div>
+
+        <SectionTitle>Skills</SectionTitle>
+        <table>
+          <tbody>
+            {data.skills.map(({ category, items }, i) => (
+              <tr key={i}>
+                <td className="pr-3 text-right align-top font-bold">{category}</td>
+                <td>{items.join(', ')}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <SectionTitle>Experience</SectionTitle>
+        {data.experience.map(({ body, ...title }, i) => (
+          <div key={i}>
+            <ItemTitle {...title} />
+            <List>
+              {body.map((text, i) => (
+                <li key={i}>{text}</li>
+              ))}
+            </List>
+          </div>
+        ))}
+
+        <SectionTitle>Education</SectionTitle>
+        {data.education.map((title, i) => (
+          <div key={i}>
+            <ItemTitle {...title} />
+          </div>
+        ))}
+      </div>
+
+      <p className="absolute inset-0 bottom-auto text-blue-400 print:text-white text-xs">
+        {data.keywords.join(', ')}
+      </p>
+    </>
+  );
+}
+
+export default Page;
+
+function SectionTitle({ children }: { children: ReactNode }) {
+  return <h1 className="pb-0.5 border-b border-gray-400 text-xl">{children}</h1>;
+}
+
+function ItemTitle({
+  company,
+  position,
+  location,
+  start,
+  end,
+}: {
+  company: string;
+  position: string;
+  location: string;
+  start: string;
+  end: string;
+}) {
+  return (
+    <div className="flex justify-between">
+      <div>
+        <p>
+          <strong>{company}</strong> &mdash; {location}
+        </p>
+        <p>
+          <em>{position}</em>
+        </p>
+      </div>
+      <p>
+        {start} &mdash; {end}
+      </p>
+    </div>
+  );
+}
+
+function List({ children }: { children: ReactNode }) {
+  return <ul className="list-disc pl-5">{children}</ul>;
+}
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Page from './Page';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Page />
+  </React.StrictMode>,
+);
